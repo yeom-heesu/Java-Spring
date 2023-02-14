4e1a31321c3802e91807b68a0b6ac0b3e25d4392
yeomheesu <2016gmltn@naver.com>
2023-02-14 22:09:13 +0900

    commit

---
+++ Chapter1/src/ch20/ContinueTest.java
@@ -0,0 +1,24 @@
+package ch20;
+
+public class ContinueTest {
+
+	public static void main(String[] args) {
+		// 1~100까지 수중 3의 배수 출력
+//		int num = 1;
+//		while (num<=100) {
+//			
+//			if (num%3==0) {
+//				System.out.println(num);
+//			}
+//			num += 1;
+//		}
+		
+		for (int i=1;i<=100;i++) {
+			if (i%3==0) {
+				System.out.println(i);
+				continue;
+			}
+		}
+	}
+
+}
