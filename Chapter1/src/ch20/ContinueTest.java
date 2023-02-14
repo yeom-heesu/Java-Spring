2675a46b8a0f618a431933e5d4e11a3f6882b7ce
yeomheesu <2016gmltn@naver.com>
2023-02-14 22:12:40 +0900

    change the code

--- Chapter1/src/ch20/ContinueTest.java
+++ Chapter1/src/ch20/ContinueTest.java
@@ -14,10 +14,10 @@ public class ContinueTest {
 //		}
 		
 		for (int i=1;i<=100;i++) {
-			if (i%3==0) {
-				System.out.println(i);
+			if (i%3!=0) {
 				continue;
 			}
+			System.out.println(i);
 		}
 	}
 
