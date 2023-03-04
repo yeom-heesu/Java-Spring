cf6396218ffc1b5a12d9049bdb39ddd8155a73f0
Heesu Yeom <43159337+yeom-heesu@users.noreply.github.com>
2023-03-04 22:15:00 +0900

    Add files via upload

--- ch2/src/main/java/com/fastcampus/ch2/MethodInfo.java
+++ ch2/src/main/java/com/fastcampus/ch2/MethodInfo.java
@@ -4,10 +4,13 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.StringJoiner;
 
+//import org.springframework.web.bind.annotation.RequestMapping;
+
 public class MethodInfo {
+//	@RequestMapping("/methodinfo1")
 	public static void main(String[] args) throws Exception{
 		//1. YoilTeller클래스의 객체를 생성
-		Class clazz = Class.forName("com.fastcampus.ch2.YoilTeller");
+		Class clazz = Class.forName("com.fastcampus.ch2.YoilTellerMVC");
 		Object obj = clazz.newInstance();
 		
 		//2. 모든 메서드 정보를 가져와서 배열에 저장
